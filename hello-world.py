66e602bfe993534d2dc5b5e838bb9000c7baac20
Jandie Hendriks <eidnaj@live.nl>
2017-04-06 13:20:51 +0200

    Machine learning hello world

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+from sklearn import tree
+
+
+# smooth = 1
+# bumpy = 0
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+
+# apple = 0
+# orange = 1
+labels = [0, 0, 1, 1]
+fruit_names = ['apple', 'orange']
+
+# train
+clf = tree.DecisionTreeClassifier()
+clf.fit(features, labels)
+
+# predict
+print fruit_names[clf.predict([[200, 0]])[0]]
+
