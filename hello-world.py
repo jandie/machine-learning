f366a1d15d9164f054a82cc834c6051569284aef
Jandie Hendriks <eidnaj@live.nl>
2017-04-06 13:33:24 +0200

    Added comments

--- hello-world.py
+++ hello-world.py
@@ -1,11 +1,12 @@
 from sklearn import tree
 
-
+# Training data:
 # smooth = 1
 # bumpy = 0
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 texture_names = ['bumpy', 'smooth']
 
+# Labels:
 # apple = 0
 # orange = 1
 labels = [0, 0, 1, 1]
